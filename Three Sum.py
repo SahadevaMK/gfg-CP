36db646fb6cfe994a008f67ec87ed87b619b81a1
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-09-29 09:27:41 +0530

    Update Three Sum.py

--- Three Sum.py
+++ Three Sum.py
@@ -15,3 +15,14 @@ for i in range(len(arr)):
             j+=1
             k-=1
 print(a)
+// brute
+
+arr = [1, 1, 0, -1, -2]
+n = len(arr)
+a = []
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if (arr[i]+arr[j]+arr[k]==0):
+                a.appprint(aend([arr[i],arr[j],arr[k]])
+print(a)
