62f3d122ffc851456990c7f765f0102e4ddf9140
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-10-02 11:30:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find first repeated character/find-first-repeated-character.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+
+    def firstRepChar(self, s):
+        # code here
+        se=set()
+        for i in s:
+            if i in se:
+                return i
+            se.add(i)
+        return -1
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        s = input()
+
+        solObj = Solution()
+
+        print(solObj.firstRepChar(s))
+# } Driver Code Ends
